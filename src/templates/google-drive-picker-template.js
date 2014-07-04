b11b3b52b6708bec69da5590a589f7eb1f5b0730
Stuart Lees <stuartlees@gmail.com>
2014-07-04 16:35:10 -0400

    Additional base files and setup
    - Base angular e2e test files
    - Base e2e HTML test file
    - Base plugin JS code
    - Applying one e2e test
    - Add HTML template
    - Add initial scss/css files (+2 squashed commit)
    
    Squashed commit:
    
    [555e3a1] Add initial dependencies
    
    - jQuery, bootstrap, angular
    
    [25c29df] Revise package file

---
+++ src/templates/google-drive-picker-template.js
@@ -0,0 +1,3 @@
+if(typeof TEMPLATES === 'undefined') {var TEMPLATES = {};}
+TEMPLATES['google-drive-picker-template.html'] = "<div></div>\n" +
+    ""; 
--- test/e2e/google-drive-picker-scenarios.js
+++ test/e2e/google-drive-picker-scenarios.js
@@ -1,9 +1,10 @@
 casper.test.begin("font size picker: size list", function (test) {
-  casper.start("http://localhost:8099/test/e2e/url-input-test.html", function () {
-    // test.assertTitle("Font Size Picker - Test Page", "Page title is the one expected");
-    // test.assertSelectorHasText(".filter-option", "18", "Default size is 18");
-    //TODO
+  casper.start("http://localhost:8099/test/e2e/google-drive-picker-scenarios.html", function () {
+    test.assertTitle("Google Drive Picker - Test Page", "Page title is the one expected");
+    // TODO: more tests
   });
+
+  // TODO: more tests
 
   casper.run(function() {
     test.done();
